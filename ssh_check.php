55aaa96bdd70d7764b883562091d6a69f3e62053
duypq2394 <39816147+duypq2394@users.noreply.github.com>
2023-09-05 09:30:55 +0900

    Update ssh_check.php

--- ssh_check.php
+++ ssh_check.php
@@ -1,3 +1,5 @@
+KeepAlivedは、起動していること
+sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@[固定IP①] systemctl is-active keepalived
 Apache(httpd)は、起動していること
 sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@[固定IP①] /etc/keepalived/chk_httpd.sh
 商用環境版SocToolServer は、起動していること
@@ -7,7 +9,10 @@ sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null
 商用環境 v4版ドキュメント管理API 発呼元10.79.32.220版SocToolServer は、起動していること
 sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@[固定IP①] /etc/keepalived/chk_SokToolServer_v410_Tougou.sh
 商用環境 連携サーバ版SocToolServer は、起動していること
-sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@[固定IP①] /etc/keepalived/chk_SokToolServer_Syanai.sh
+sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@[固定IP①] /bin/curl --connect-timeout 1 --output - 'http://[固定IP③]/'
+Web2からチェック：10.79.32.111
+Web1からチェック：10.79.32.112
+
 NFS(nfs-server)は、起動していること
 sudo -S /usr/local/bin/sshpass -p '!qaz2wsx' ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@[固定IP①] /etc/keepalived/chk_nfs-server.sh
 
